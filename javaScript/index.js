fde6dbf99865c6d63dc5f9908abeada8c0970383
CalculadoraIMC <vitorreis0303@gmail.com>
2022-10-03 13:29:40 -0300

    Criando JS para funcionalidade do Adicionar Atividade dentro de um array

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,45 +1,105 @@
-let data = [
-]
+    let data = {
+         arraySegunda : [
+            {
+                card : 'vitor',
+                horario : '02:00',
+                indice : 1,
+            },
+            {
+                card : 'vitor 2',
+                horario : '02:00',
+                indice : 2,
+            }
+         ],
+         arrayTerca : [],
+         arrayQuarta : [],
+         arrayQuinta : [],
+         arraySexta : [],
+         arraySabado : [],
+         arrayDomingo : [],
+    };
 
-const criarCard = (data, indice) => {
+    
+    localStorage.setItem('data', JSON.stringify(data));
+    data = JSON.parse(localStorage.getItem('data'))
 
-    const horario = document.createElement('li');
-    horario.innerHTML = `
-    <div class="planner-horario-interno__card">
-        <h2 class="horario-interno__card">${data.horario}</h2>
-    </div>
-    `
-    document.getElementById('containerHorarios').appendChild(horario);
+    const criarCard = (element) => {
+    
+        const button = element.target.getAttribute('id-day');
+        console.log(button)
 
-    const card = document.createElement('li');
-    card.classList.add('container-cards-interno__main');
-    card.innerHTML = `
-        <div class="card__planner" id="cardItem">
-            <div class="border-card__main"></div>
-            <div class="texto-button__card-interno">
-                <p class="texto-card__main" id="textoDigitado">${data.card}</p>
-                <input class="button-card__main" type="button" value="Apagar" data-indice=${indice}></input>
+        let dia = [];
+
+        if(button == "segunda"){
+            dia = [...data.arraySegunda];
+        }
+        if(button == "terca"){
+            dia = [...data.arrayTerca];
+        }
+        if(button == "quarta"){
+            dia = [...data.arrayQuarta];
+        }
+        if(button == "quinta"){
+            dia = [...data.arrayQuinta];
+        }
+        if(button == "sexta"){
+            dia = [...data.arraySexta];
+        }
+        if(button == "sabado"){
+            dia = [...data.arraySabado];
+        }
+        if(button == "domingo"){
+            dia = [...data.arrayDomingo];
+        }
+        
+        const containerCard = document.getElementById('containerCards');
+        containerCard.innerHTML = '';
+
+        const containerHorario = document.getElementById('containerHorarios');
+        containerHorario.innerHTML = '';
+    
+        dia.forEach(containerDia => {
+            const horario = document.createElement('li');
+            horario.innerHTML = `
+            <div class="planner-horario-interno__card">
+                <h2 class="horario-interno__card">${containerDia.horario}</h2>
             </div>
-        </div>
-    `
-    document.getElementById('containerCards').appendChild(card);
-}
+            `
+            document.getElementById('containerHorarios').appendChild(horario);
+        
+            const card = document.createElement('li');
+            card.classList.add('container-cards-interno__main');
+            card.innerHTML = `
+                <div class="card__planner" id="cardItem">
+                    <div class="border-card__main"></div>
+                    <div class="texto-button__card-interno">
+                        <p class="texto-card__main" id="textoDigitado">${containerDia.card}</p>
+                        <input class="button-card__main" type="button" value="Apagar" data-indice=${containerDia.indice}></input>
+                    </div>
+                </div>
+            `
+            containerCard.appendChild(card);
+        })
 
-const limparCards = () => {
-    const containerCards = document.getElementById('containerCards');
-    while(containerCards.firstChild){
-        containerCards.removeChild(containerCards.lastChild);
     }
 
-    const containerHorarios = document.getElementById('containerHorarios');
-    while(containerHorarios.firstChild){
-        containerHorarios.removeChild(containerHorarios.lastChild);
-    }
-}
+    document.querySelector('.dia-segunda__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-terca__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-quarta__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-quinta__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-sexta__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-sabado__color').addEventListener('click', criarCard);
+    document.querySelector('.dia-domingo__color').addEventListener('click', criarCard);
+
 
 const refreshScreen = () => {
-    limparCards();
-    data.forEach ((textoCard, indice) => criarCard(textoCard, indice));
+    //limparCards();
+    for (const dia in data) {
+        if (data.hasOwnProperty.call(data, dia)) {
+            const element = data[dia];
+            
+        }
+    }
 }
 
 refreshScreen();
@@ -53,14 +113,55 @@ const cadastrarNovaTarefa = () => {
 
     const horario = optionText.text;
 
-        data.push({'card': textoCard, horario});
-        refreshScreen();
+    const selectDia = document.querySelector('#diaSemana');
+    const optionDia = selectDia.options[selectDia.selectedIndex];
+
+    const dia = optionDia.text;
+
+    if(dia == 'Segunda-feira'){
+        data.arraySegunda.push({'card': textoCard, horario})
+        //refreshScreen();
         inputAtividade.value = '';
+    } else if(dia == 'Terça-feira'){
+        data.arrayTerca.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
+    else if(dia == 'Quarta-feira'){
+        data.arrayQuarta.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
+    else if(dia == 'Quinta-feira'){
+        data.arrayQuinta.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
+    else if(dia == 'Sexta-feira'){
+        data.arraySexta.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
+    else if(dia == 'Sábado'){
+        data.arraySabado.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
+    else if(dia == 'Domingo'){
+        data.arrayDomingo.push({'card': textoCard, horario, dia});
+        //refreshScreen();
+        inputAtividade.value = '';
+    }
 }
 
-const removerItem = (indice) => {
-    data.splice(indice, 1);
-    refreshScreen();
+const removerItem = (index) => {
+    for (const dia in data) {
+      data[dia].forEach((cdia, i) => {
+        if(cdia.indice == index){
+            data[dia].splice(i,1);
+        }
+      })
+    }
 }
 
 function clickItem (evento) {
